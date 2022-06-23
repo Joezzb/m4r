b317d0d1991343da54a1a390312775e39cf4752b
Joezzb <joezzb@163.com>
2022-06-23 03:27:13 +0100

    upload code ver0

---
+++ train/README.md
@@ -0,0 +1,1 @@
+This folder contains the standard training code with five-fold cross validation. \\
---
+++ train/package.py
@@ -0,0 +1,47 @@
+from tqdm import tqdm
+import os
+import time
+from random import randint
+
+import numpy as np
+from scipy import stats
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.model_selection import KFold
+
+import nibabel as nib
+import pydicom as pdm
+import nilearn as nl
+import nilearn.plotting as nlplt
+import h5py
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.animation as anim
+import matplotlib.patches as mpatches
+import matplotlib.gridspec as gridspec
+
+import seaborn as sns
+import imageio
+from skimage.transform import resize
+from skimage.util import montage
+
+from IPython.display import clear_output
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.nn import MSELoss
+
+
+import warnings
+warnings.simplefilter("ignore")
+import matplotlib.ticker as ticker
